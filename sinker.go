0025443c073486e4601ac8a1a7d7cb4f52d82085
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2021-02-09 19:34:24 +0200

    fix

--- sinker.go
+++ sinker.go
@@ -31,11 +31,6 @@ type Config struct {
 	Gist
 }
 
-type Person struct {
-	FirstName string `json: "firstName"`
-	LastName  string `json: "lastName"`
-	Children  []string
-}
 type Conf struct {
 	Gist Gist
 }
@@ -47,30 +42,19 @@ func parseJsonConfg(data []byte) (Config, error) {
 	return config, err
 }
 func main() {
-	j := []byte(`{"lastName": "Cohen",  "firstName":"Aaron",  "children": ["Jesse"]}`)
-	var person Person
-	err := json.Unmarshal(j, &person)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(person.FirstName)
-	fmt.Println(person.LastName)
-	fmt.Println(person.Children)
 	k := []byte(`{"gist": {"accessToken": "xxx", "files": ["~/.bashrc"]}}`)
 	var c Conf
-	err = json.Unmarshal(k, &c)
+	err := json.Unmarshal(k, &c)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(c.Gist.AccessToken)
-
 	fmt.Println(c.Gist.Files)
 	data, err := readSinkerRc()
 	fmt.Println(string(data))
 	if err != nil {
 		log.Fatal("Problem reading your .sinkerrc.json file: " + err.Error())
 	}
-	//fmt.Println(string(data))
 	config, err := parseJsonConfg(data)
 	if err != nil {
 		log.Fatal(err)
