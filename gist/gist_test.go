1fe70f6c76d428e63d82ed79cc35492b1534b238
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2021-02-19 11:29:21 +0200

    add mocked test

--- gist/gist_test.go
+++ gist/gist_test.go
@@ -3,10 +3,19 @@ package gist
 import (
 	"os"
 	"testing"
+
+	"github.com/jarcoal/httpmock"
 )
 
 func TestGet(t *testing.T) {
-	// Access token, gist ID
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	// Intercept http request to github.
+	httpmock.RegisterResponder("GET", "https://api.github.com/gists/142a4dfb66f0e2eab38cb68e0b69d95c",
+		httpmock.NewStringResponder(200, `{"files": {".bashrc": {"filename": ".bashrc"}}}`))
+
+	// Pass Access token and gist ID
 	gist, resp, err :=
 		Get(os.Getenv("SINKER_GIST_ACCESS_TOKEN"), "142a4dfb66f0e2eab38cb68e0b69d95c")
 	if err != nil {
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/google/go-github/v33 v33.0.0
+	github.com/jarcoal/httpmock v1.0.8
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 )
