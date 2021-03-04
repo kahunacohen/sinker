f6ed60101c2ee75e1737584d09e97da49693226b
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2021-03-04 16:39:37 +0200

    exract func

--- sinker.go
+++ sinker.go
@@ -10,6 +10,18 @@ import (
 	"github.com/kahunacohen/sinker/gist"
 )
 
+func doIt(config conf.Conf, file conf.File) {
+	fh, err := os.Open(file.Path)
+	if err != nil {
+		log.Fatalf("problem reading file: %s", err)
+	}
+	resp := gist.Sync(config.Gist.AccessToken, fh, file.Id)
+	if resp.Error != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Println(resp.LocalModLast)
+}
+
 func main() {
 
 	config, err := conf.Get("/Users/acohen/.sinkerrc.json")
@@ -18,15 +30,7 @@ func main() {
 	}
 
 	for _, file := range config.Gist.Files {
-		fh, err := os.Open(file.Path)
-		if err != nil {
-			log.Fatalf("problem reading file: %s", err)
-		}
-		resp := gist.Sync(config.Gist.AccessToken, fh, file.Id)
-		if resp.Error != nil {
-			log.Fatalf(err.Error())
-		}
-		fmt.Println(resp.LocalModLast)
+		doIt(*config, file)
 
 	}
 }
